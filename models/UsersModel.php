145059c072de7cb17d77eab96e94c290740331b0
ariyuda08 <220040126@stikom-bali.ac.id>
2024-06-16 13:35:35 +0800

    Added getUserById method to retrieve user data by id

--- models/UsersModel.php
+++ models/UsersModel.php
@@ -23,6 +23,19 @@ class UsersModel
         }
     }
 
+    public function getUserById($id)
+    {
+        try {
+            $query = "SELECT * FROM " . $this->table_name . " WHERE id_user = ?";
+            $stmt = $this->conn->prepare($query);
+            $stmt->bindParam(1, $id);
+            $stmt->execute();
+            return $stmt->fetch(PDO::FETCH_ASSOC);
+        } catch (PDOException $e) {
+            echo "Error: " . $e->getMessage();
+        }
+    }
+
     public function addUsers($data)
     {
         try {
